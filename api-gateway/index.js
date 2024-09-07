dd1287d250ca2500536a3f4ad23dfd8a93ceaf99
Sehan Ranaweera <sehanranaweera2002@gmail.com>
2024-09-07 15:11:52 +0530

    implement api gateway.

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -3,6 +3,20 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`API Gateway running on port ${PORT}`);
+app.use("/auth", createProxyMiddleware({ target: "http://auth-service:3001", changeOrigin: true }));
+app.use(
+  "/transactions",
+  createProxyMiddleware({ target: "http://transaction-service:3002", changeOrigin: true })
+);
+app.use(
+  "/monitoring",
+  createProxyMiddleware({ target: "http://monitoring-service:3003", changeOrigin: true })
+);
+app.use(
+  "/notify",
+  createProxyMiddleware({ target: "http://notification-service:3004", changeOrigin: true })
+);
+
+app.listen(port, () => {
+  console.log(`API Gateway running on port ${port}`);
 });
